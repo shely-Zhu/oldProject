190b75c5d3d3e21d80ff6dc243282ea0bb29defc
chengqingqing <chegnqingqing@chtwm.com>
2019-03-28 16:51:08 +0800

    表单-财商

--- src/common/js/components/config/content.js
+++ src/common/js/components/config/content.js
@@ -5,4 +5,8 @@
 module.exports = function() {
     // 首页banner
     this.findBannerByPosition_api = http_url.content_url + '/frontend/findBannerByPosition';
+    // 上传图片
+    this.uploadApi = http_url.content_url + '/file/mount/upload';
+    // 图片下载
+    this.downloadApi = http_url.content_url + '/file/mount/download';
 }
