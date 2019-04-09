3a48c702c8a3f1d97d8ad08f3ff39f92511da215
陈宁宁 <chenningning@chtwm.com>
2019-04-09 13:30:51 +0800

    去掉注释

--- src/common/js/ajaxLoading.js
+++ src/common/js/ajaxLoading.js
@@ -133,7 +133,7 @@ var splitUrl = require('./components/splitUrl.js')();
 
             //发送ajax请求
             var ajaxFunc = function(obj) {
-                document.cookie = "APPSESSIONID=3c20905e-11d1-41f8-b664-1db50f3e6107;domain="+window.location.hostname+";path=/"
+                // document.cookie = "APPSESSIONID=3c20905e-11d1-41f8-b664-1db50f3e6107;domain="+window.location.hostname+";path=/"
                 var ajax = $.Deferred(); //声明一个deferred对象
 
                 //设置ajax请求的contentType  data数据添加JSON.stringify
