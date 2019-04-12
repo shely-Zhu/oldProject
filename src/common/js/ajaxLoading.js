c9fff5b8d33159108540ef67564e45f643636e3f
chengqingqing <chegnqingqing@chtwm.com>
2019-04-12 14:07:29 +0800

    ajaxLoading中状态码4008改完4007

--- src/common/js/ajaxLoading.js
+++ src/common/js/ajaxLoading.js
@@ -129,7 +129,7 @@ var splitUrl = require('./components/splitUrl.js')();
             $.each(param, function(i, el) {
                 obj.push($.extend({}, defaults, el));
             })
-            document.cookie = "APPSESSIONID=607cd608-89d2-4fbf-8d98-320e05064009;domain="+window.location.hostname+";path=/"
+            document.cookie = "APPSESSIONID=2e82dd39-d4a7-4838-a237-8d04f8f82dc2;domain="+window.location.hostname+";path=/"
 
             //发送ajax请求
             var ajaxFunc = function(obj) {
@@ -260,7 +260,7 @@ var splitUrl = require('./components/splitUrl.js')();
                                 //防止window.location.href在执行完请求里的所有代码之后再跳转
                                 throw 'jump login';
                                 return false;
-                            } else if (data.status == '4008') {
+                            } else if (data.status == '4007') {
                                 // 其他黑名单接口未登录，跳转data.data
                                 manualTriggerLogin.locationFunc(data);
                                 //防止window.location.href在执行完请求里的所有代码之后再跳转
