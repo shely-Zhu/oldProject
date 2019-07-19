322b8ef95a71fffb9da98dce98fec307188e3d6d
shiyunrui <576486756@qq.com>
2019-07-19 16:36:57 +0800

    史芸瑞提交：修复jjs资产列表接口名定义

--- src/commonResources/static/js/jjsAssets.js
+++ src/commonResources/static/js/jjsAssets.js
@@ -105,7 +105,7 @@ $(function() {
                     tipAction(data.msg);
                 }
             }, {
-                url: site_url.totalAssets_api, //查询总资产 从中拿到jjs的资产
+                url: site_url.totalAssets_appApi, //查询总资产 从中拿到jjs的资产
                 data: {},
                 callbackDone: function(json) {
                     console.log(JSON.stringify(json));
