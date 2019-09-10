860bb2b3c2768654e079eabb9076fe81ed178dca
fangyangfan <tmp-fangyangfan@chtwm.com>
2019-09-10 18:26:24 +0800

    新增

---
+++ data/web/account/standardPoor/getTotalAssets.js
@@ -0,0 +1,24 @@
+/*
+
+  获取总资产
+
+*/
+
+// 使用 Mock
+var Mock = require('mockjs');
+
+var mymessage = Mock.mock({
+    "data": {
+      "consumptionAssets": "100,000.00",
+      "consumptionProportion": "50.55",
+      "guaranteeAssets": "25,000.00",
+      "guaranteeProportion": "12.45",
+      "valueAddedAssets": "35.000.00",
+      "valueAddedProportion": "16.25",
+      "valuePreservingAssets": "45.000.00",
+      "valuePreservingProportion": "20.75"
+    },
+    "message":"操作成功！",
+    "status":"0000"
+});
+module.exports=mymessage;
