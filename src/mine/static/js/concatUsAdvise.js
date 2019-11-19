1e56b43931657b567c34ffe8fc68ecdc24cdb580
chentiancheng <1934024095@qq.com>
2019-11-19 13:41:03 +0800

    提交代码，修改意见反馈

--- src/mine/static/js/concatUsAdvise.js
+++ src/mine/static/js/concatUsAdvise.js
@@ -17,7 +17,41 @@ var tipAction = require('@pathCommonJs/components/tipAction.js');
 var splitUrl = require('@pathCommonJs/components/splitUrl.js')();
 var generateTemplate = require('@pathCommonJsComBus/generateTemplate.js');
 $(function() {
-    var that = this;
-    uploadFile(that.asyncAll, that, 1); //插件初始化
+    let concatUsAdvise={
+        gD: {
+            feedbackType:null,//客户反馈类型
+            imgNum: 0, // 需要上传的图片数量
+            idArr: [], // 已上传的id数组
+            feedbackDesc:null,//客户反馈描述
+        },
+        init:function(){
+            var that=this;
+            $(".list").eq(0).hide()
+            uploadFile(that.asyncAll, that, 1); //插件初始化
+            that.event()
+        },
+        // 所有图片上传完毕，请求申请投资者分类接口
+        asyncAll: function(idJson, idTypeArr) {
+            var that = this;
+            that.gD.imgNum = $('.uploadMaterial').find('.imgNum').length;
+            that.gD.idArr.push.apply(that.gD.idArr, idJson.data.fileId);
+            if(that.gD.idArr.length === that.gD.imgNum){
+                console.log(1)
+            }
+            console.log(idJson)
+            debugger
+            // alert(1)
+        },
+        event:function(){
+            var that =this;
+            $(".list").on("click",function(e){
+             that.gD.feedbackType = $(this).children("input").val() || 0
+            })
+        },
+        submitAdvise(){
+            alert(1)
+        }
+    }
+    concatUsAdvise.init()
     
 })
