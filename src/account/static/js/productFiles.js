8734703858ff90a868943b8a885c807a5dc85842
webwangjia <986785991@qq.com>
2019-11-20 15:00:34 +0800

    产品档案

--- src/account/static/js/productFiles.js
+++ src/account/static/js/productFiles.js
@@ -12,3 +12,25 @@ require('@pathCommonJs/ajaxLoading.js');
 var tipAction = require('@pathCommonJs/components/tipAction.js');
 var splitUrl = require('@pathCommonJs/components/splitUrl.js')();
 var generateTemplate = require('@pathCommonJsComBus/generateTemplate.js');
+$(function() {
+  var somePage = {
+    $e: {
+  
+    },
+    gV: { // 全局变量
+       
+    },
+
+  }
+  $(".openOff").click(function(){
+      if($(".openOff").text() != "收起"){
+        $(".productCostDetail").addClass("openStyle")
+        $(".productCostDetail").removeClass("productCostDetail");
+        $(".openOff").text("收起")
+      }else{
+        $(".openStyle").addClass("productCostDetail")
+        $(".openStyle").removeClass("openStyle ");
+        $(".openOff").text("展开")
+      }
+  }) 
+})
