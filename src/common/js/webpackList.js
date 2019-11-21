b42ea9a81f953d3be8876a4f188a0f5e6bd9e5e1
chenningning <chenningning@chtwm.com>
2019-11-21 14:30:40 +0800

    添加现金宝的详情的页面

--- src/common/js/webpackList.js
+++ src/common/js/webpackList.js
@@ -286,4 +286,6 @@ module.exports = {
     './account/static/js/public/transactionRecords': './src/account/static/js/public/transactionRecords.js',
     // 自选公募交易明细
     './account/static/js/public/transactionDetail': './src/account/static/js/public/transactionDetail.js',
+    // 现金宝详情页
+     './account/static/js/public/cashTreasure': './src/account/static/js/public/cashTreasure.js',
 }
