d181b17731a3913d5e46f2c0f9ed4b2836459ac4
peicongcong <2256757696@qq.com>
2019-11-25 11:01:26 +0800

    '上拉加载结构变化'

--- src/common/js/webpackList.js
+++ src/common/js/webpackList.js
@@ -252,7 +252,7 @@ module.exports = {
     //已确认交易 公共js
     './account/static/js/common/transcationTem': './src/account/static/js/common/transcationTem.js',
     //待确认交易
-    // './account/static/js/private/tobeConfirmTransaction': './src/account/static/js/private/tobeConfirmTransaction.js',
+    './account/static/js/private/tobeConfirmTransaction': './src/account/static/js/private/tobeConfirmTransaction.js',
     //收益分配明细
     './account/static/js/private/incomeDistribution': './src/account/static/js/private/incomeDistribution.js',
     //公募资产
@@ -298,5 +298,5 @@ module.exports = {
     // 基金确认书
      './account/static/js/private/privateFundPdf':   './src/account/static/js/private/privateFundPdf.js',
     //我的定投_定投明细
-    './Financial/static/js/publicPlacement/castSurely_details' : './Financial/static/js/publicPlacement/castSurely_details.js' 
+    './Financial/static/js/publicPlacement/castSurely_details': './Financial/static/js/publicPlacement/castSurely_details.js'
 }
