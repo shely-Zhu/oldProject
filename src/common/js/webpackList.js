9014d5003b85dd90f61da0b7e8de7eab9d38f86c
caoqihai <137594357@qq.com>
2019-11-25 11:42:20 +0800

    提交静态页

--- src/common/js/webpackList.js
+++ src/common/js/webpackList.js
@@ -311,7 +311,7 @@ module.exports = {
     //我的定投计划
     './Financial/static/js/publicPlacement/myInvestmentPlan' : './src/Financial/static/js/publicPlacement/myInvestmentPlan.js',
     //普通基金产品详情页_定投
-    './Financial/static/js/ordinarySetThrow' : './src/Financial/static/js/ordinarySetThrow.js',
+    './financial/static/js/ordinarySetThrow' : './src/financial/static/js/ordinarySetThrow.js',
 
     // 现金管理
     './Financial/static/js/cashManagement' : './src/Financial/static/js/cashManagement.js',
