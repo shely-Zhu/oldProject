33a08c78e1baae11d3228bb4f688cf590091367e
乐媛 <1192439205@qq.com>
2019-11-26 16:55:33 +0800

    add 定投详情

--- src/financial/static/js/publicPlacement/castSurelyDetails.js
+++ src/financial/static/js/publicPlacement/castSurelyDetails.js
@@ -18,7 +18,12 @@ require('@pathCommonJs/components/headBarConfig.js');
 var tipAction = require('@pathCommonJs/components/tipAction.js');
 var generateTemplate = require('@pathCommonJsComBus/generateTemplate.js');
 var splitUrl = require('@pathCommonJs/components/splitUrl.js');
-
+//获取地址栏参数
+getQueryString = function (name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]); return '';
+}
 $(function () {
 
   var regard = {
@@ -32,21 +37,70 @@ $(function () {
 
     },
     getData: function () {
-      console.log(111);
 
       var that = this;
-      var scheduledProtocolId = 11
+      var scheduledProtocolId = location
       //请求页面数据
       var obj = [{
         url: site_url.pofFixedDetail_api,
         data: {
-          scheduledProtocolId: scheduledProtocolId
+          scheduledProtocolId: getQueryString('scheduledProtocolId')
         },
         callbackDone: function (json) {
+          console.log(json);
+
+          json = json.data
+          $('.fundName').html(json.fundName);
+          $('.balanceMask').html(json.balanceMask);
+          $('.totalTradeTimes').html(json.totalTradeTimes);
+          $('.totalCfmBalaMask').html(json.totalCfmBalaMask);
+          $('.nextFixrequestDateMask').html(json.nextFixrequestDateMask);
+          $('.fixedPeriodMask').html(json.fixedPeriodMask);
+          $('.capitalModeDesc').html(json.capitalModeDesc);
+          $('.bankName').html(json.bankName);
+          $('.bankAccountMask').html(json.bankAccountMask);
+          $('.signDate').html(json.signDateMask);
+          $('.bankNo').html(json.bankNo);
+          $('.bankThumbnailUrl').attr('src', json.bankThumbnailUrl);
+          $('.totalCfmShareMask').html(json.totalCfmShareMask);
+          $('.serviceCharge').html('含手续费' + json.serviceCharge + '元');
+          var fixState, str
+          switch (json.fixState) {
+            case 'A':
+              fixState = "进行中"
+              str = '<div>终止</div> <div class="cen ">暂停</div> <div class="active ">修改</div>'
+              break;
+
+            case 'H':
+              fixState = "已终止"
+              str = ""
+              break;
+
+            case 'P':
+              fixState = "暂停"
+              str = '<div >终止</div> <div class="active ">续保</div>';
+              break;
+            case 'D':
+              fixState = "删除"
+              break;
+            case 'F':
+              fixState = "签约失败"
+              break;
+
+            default:
+              break;
+          }
+          $('.fixState').html(fixState);
+          $('.footer').html(str);
           var tplm = $("#dataLists").html();
           var template = Handlebars.compile(tplm);
-          var html = template(json.data.pageList);
-          $("#cashListCon").html(html);
+          var tradeRecord = json.tradeRecord
+          tradeRecord.forEach(n => {
+            n.tradeTime = n.tradeTime.split(" ")[0]
+            n.status = n.status === "1" ? 1 : 0
+          });
+          var html = template(tradeRecord);
+          $(".tplBox").html(html);
 
         },
         callbackFail: function (json) {
@@ -76,6 +130,7 @@ $(function () {
       //   that.getReport();
       // });
     },
+
 
   }
 
