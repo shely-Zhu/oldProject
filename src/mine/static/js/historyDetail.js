b64260e214f99f154028fedadfa88ad0db96d6c7
yangjinlai <274948052@qq.com>
2019-11-28 17:41:15 +0800

    多余文件去掉

--- src/mine/static/js/historyDetail.js
+++ src/mine/static/js/historyDetail.js
@@ -1,6 +1,6 @@
 
 require('@pathIncludJs/base.js');
-require('@pathCommonJs/components/utils.js');
+
 require('@pathCommonJs/components/headBarConfig.js');
 require('@pathCommonJs/ajaxLoading.js');
 
