fdba72f977c79437fd97b4a8400b3624e9f04aa2
liliang <ajaxjsonp@163.com>
2019-12-03 15:00:16 +0800

    修改财富学堂样式

--- src/homePage/static/js/fortuneCollegeList.js
+++ src/homePage/static/js/fortuneCollegeList.js
@@ -143,6 +143,7 @@ var productPublic = {
               generateTemplate(listContent,$('.forumList'),$('#forumContent'));	
               setTimeout(()=>{
                 $('.broadcast').find('.bigspan').eq(0).addClass('getColor');
+                $('.broadcast').find('.bigspan').eq(0).css({"paddingLeft":0,"borderLeft":'none'});
               },100)
               mui('.mui-scroll-wrapper').scroll({
                 deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006 
