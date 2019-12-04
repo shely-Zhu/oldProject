7a2a96cd818e76b5f696b7d6b91e7892fd548ddd
yanan <1004596099@qq.com>
2019-12-04 16:22:09 +0800

    bug 修改

--- src/financial/static/js/publicPlacement/cashTransformOut.js
+++ src/financial/static/js/publicPlacement/cashTransformOut.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-11-26 14:42:56
- * @LastEditTime: 2019-11-30 16:55:53
+ * @LastEditTime: 2019-12-04 16:13:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \htjf-app\src\financial\static\js\publicPlacement\cashTransformOut.js
@@ -89,6 +89,7 @@ $(function () {
 			el_defaultBankName:$(".defaultBankName"), //默认银行卡名称
 			el_defaultBankCode:$(".defaultBankCode"), //默认银行卡号后四位
 			el_defaultBankImgUrl:$(".defaultBankImgUrl"), //默认银行logo
+			el_maxTranMoney:$(".maxMoney") //赎回金额
 			
 		},
 
@@ -119,7 +120,6 @@ $(function () {
 		   that.gv.fundCode = data.fundCode;
 		   that.gv.transformTotalMoney = data.money;
 		   that.gv.transformMoney = data.money;
-		   
 
 		 },
 		 initElement:function(){
@@ -181,7 +181,7 @@ $(function () {
 					 that.$e.el_defaultBankName[0].textContent =defaultCarData.bankName;
 					 that.$e.el_defaultBankImgUrl.attr("src",defaultCarData.bankThumbnailUrl);
 					 that.$e.el_defaultBankCode[0].textContent =defaultCarData.bankAccountMask.substr(-4);
-                     that.$e.el_singleNum[0].textContent = defaultCarData.singleNumMask;
+                     that.$e.el_singleNum[0].textContent = defaultCarData.availableShare;
 					 that.$e.el_oneDayNum[0].textContent = defaultCarData.oneDayNumMask;
 					 generateTemplate(that.gv.cashList, that.$e.TransferFundsContent, that.$e.templateTransferFunds);
 				 }
@@ -278,7 +278,7 @@ $(function () {
 			   that.gv.defaultTradeAcco = $(this).attr("tradeAcco");  // 默认交易账号
 			   that.gv.defaultCapitalMode = $(this).attr("capitalMode"); // 默认资金方式
 			 
-			   that.$e.el_singleNum[0].textContent = $(this).attr('singleNumMask');
+			   that.$e.el_singleNum[0].textContent = $(this).attr('availableShare');
 			   that.$e.el_oneDayNum[0].textContent = $(this).attr('oneDayNumMask');
 			   
 			   that.$e.el_defaultBankName[0].textContent = $(this).attr("bankName");
