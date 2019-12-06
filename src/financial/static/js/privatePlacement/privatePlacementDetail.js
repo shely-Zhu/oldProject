1cdaa36a675b348b017bcf7f3fc5dbf10b8fa26b
tianjunguo <tianjunguo@chtwm.com>
2019-12-06 18:47:41 +0800

    解决密码滚动不到可视区域的问题

--- src/financial/static/js/privatePlacement/privatePlacementDetail.js
+++ src/financial/static/js/privatePlacement/privatePlacementDetail.js
@@ -510,7 +510,7 @@ $(function(){
 
 				},
 				callbackFail: function(json){  //失败后执行的函数
-					tipAction(json.msg);
+					tipAction(json.message);
 
 				}
 			}];
@@ -541,7 +541,7 @@ $(function(){
 
 				},
 				callbackFail: function(json){  //失败后执行的函数
-					tipAction(json.msg);
+					tipAction(json.message);
 
 				}
 			}];
@@ -686,7 +686,7 @@ $(function(){
 							}
 						},
 						callbackFail: function(json){  //失败后执行的函数
-							tipAction(json.msg);
+							tipAction(json.message);
 							that.data.canClick = true;//变为可点击
 		
 						}
