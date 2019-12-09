92f9b5d87f0f224c98b7974014582105e9fa7c0f
蔡文琦 <215312925@qq.com>
2019-12-09 14:40:04 +0800

    提交代码 老带新二维码

--- src/recommend/static/js/recommend.js
+++ src/recommend/static/js/recommend.js
@@ -54,6 +54,7 @@ $(function() {
             title: '', // 微信分享title设置
             imageUrl: '', // 微信分享图片设置
             introduction: '', // 微信分享简介设置
+            // advisor:[],
         },
         list: [], // 存放接口里获取的理财师数据
         phone: '', // 存放用户信息接口里取出来的脱敏手机号
@@ -126,7 +127,6 @@ $(function() {
         },
         // 初始页面请求
         initialPage: function() {
-            console.log('initialPage')
             var that = this;
             // 理财师接口传参
             var custBroData = {
@@ -176,8 +176,7 @@ $(function() {
 
             // 获取理财师的接口
             that.generateAjaxObj(site_url.custBro_api, custBroData, function(data) {
-                console.log('获取理财师的接口callback')
-                    // 根据理财师处理页面逻辑
+                // 根据理财师处理页面逻辑
                 that.dealManagerLogic(data);
             }, function() {
                 //设置立即邀请好友的按钮状态为不可点
@@ -203,22 +202,25 @@ $(function() {
         dealManagerLogic: function(data) {
             var that = this,
                 shareUrl = '', // 分享出去链接
-                existMain = data.existMain,
+                existMain = data.existMain;
                 advisor = data.advisor;
-            if (existMain == 1 && advisor.length > 1) {
+                
+            if (existMain == 0 && advisor.length > 1) {
                 $('.recommendLcsText').html("您的理财师：")
                 //无专属且理财师多于1位默认展示一个
                 $('.manager_show').html(advisor[0].codeName + advisor[0].empNo)
                 that.generateShareLink(advisor[0].empNo);
                 that.list=advisor
-                debugger
+                // debugger
                 generateTemplate(advisor,that.getElements.popupUl, that.getElements.bankListTemplate,true);
-                $(".popup").show();
+                $('.recommendLcs').css('pointer-events','')
+                // $(".popup").show();
             } else {
                 // 有专属理财师或者只有一位普通理财师
                 $('.recommendLcsText').html("您的理财师：")
                 $('.manager_show').html(advisor[0].codeName + advisor[0].empNo)
                 that.generateShareLink(advisor[0].empNo);
+                $('.recommendLcs').css('pointer-events','none')
             }
         },
 
@@ -227,7 +229,6 @@ $(function() {
          * @author songxiaoyu 2018-07-18
          */
         generateShareLink: function(num) {
-            console.log('generateShareLink')
             var that = this,
                 aesEncrypt = ''; // 加密信息
 
@@ -247,12 +248,12 @@ $(function() {
                             window.location.href = site_url.realNameStepOne_url;
                         })
                     } else {
+                        $(".recommendBoxUserName").html(json.data.oldName)
                         // 已实名认证
                         aesEncrypt = json.data.aesEncrypt;
-
                         //拼分享出去的链接
                         shareUrl = site_url.newRecommend_url + '?url=' + aesEncrypt;
-
+                        
                         // 生成二维码
                         that.generateQrcode(shareUrl)
 
@@ -391,13 +392,13 @@ $(function() {
             var that = this;
 
             // 清楚已有二维码
-            $('#code').html("");
+            $('.recommendQRCode').html("");
 
             //计算二维码的宽高
-            var w = $('#code').width();
+            var w = $('.recommendQRCode').width();
 
             //调用生成二维码组件
-            jQuery("#code").qrcode({
+            jQuery(".recommendQRCode").qrcode({
                 //render: "table", //table方式 
                 render: 'image',
                 width: w,
@@ -410,42 +411,43 @@ $(function() {
         },
         events: function() {
             var that = this;
-
-            //点击选择理财师
-            mui("body").on('tap', '.popup-li', function() {
-                // //出现选择器
-                // debugger
-                // popPicker(1, that.list, $('.mui-table-view a'));
-                console.log(that.list)
-            })
             //点击取消选择
-            
+            mui("body").on('tap', '.recommendLcs', function() {
+                $(".popup").show()
+            })
+            //点击选择理财师
+            mui("body").on('tap', '.popup-li', function(e) {
+                // 拿到当前点击的理财师的名称和工号
+                $(".manager_show").html($(this).text())
+                $(".popup").hide()
+                that.generateShareLink(that.list[$(this).index()].empNo);
+            })
             mui("body").on('tap', '.cancel', function() {
                $(".popup").hide();
             })
 
             //点击--生成专属二维码
-            mui("body").on('tap', '.generate_btn', function() {
-                //判断理财师是否已选择 
-                if (!$('.mui-table-view .mui-navigate-right').hasClass('hasSelect') && that.list.length) {
-                    tipAction('请选择您的理财师');
-                    return false;
-                }
+            // mui("body").on('tap', '.generate_btn', function() {
+            //     //判断理财师是否已选择 
+            //     if (!$('.mui-table-view .mui-navigate-right').hasClass('hasSelect') && that.list.length) {
+            //         tipAction('请选择您的理财师');
+            //         return false;
+            //     }
 
-                //获取选中的理财师工号
-                var num = $('.mui-table-view a').attr('num');
+            //     //获取选中的理财师工号
+            //     var num = $('.mui-table-view a').attr('num');
 
-                //按钮变色
-                var $this = $(this).find('.txt');
-                $this.addClass('disable').attr('disabled', 'disabled');
+            //     //按钮变色
+            //     var $this = $(this).find('.txt');
+            //     $this.addClass('disable').attr('disabled', 'disabled');
 
-                // 清楚原有二维码
-                // jQuery("#code").qrcode.clear();
-                // 生成新的二维码
-                that.generateShareLink(num)
-                    // 二维码显示
-                    // that.getElements.qrcode_wrap.show();
-            })
+            //     // 清楚原有二维码
+            //     // jQuery("#code").qrcode.clear();
+            //     // 生成新的二维码
+            //     that.generateShareLink(num)
+            //         // 二维码显示
+            //         // that.getElements.qrcode_wrap.show();
+            // })
 
             //点击隐藏弹层
             // $('.shareMask').on('click', function(e) {
@@ -538,7 +540,6 @@ $(function() {
          */
         asyncAll: function() {
             var that = this;
-            console.log(that.setting.num);
             if (++that.setting.count === that.setting.num) {
                 that.weixinShare();
             }
