58a19cd8edd1ff1892ce0e1ffec75aad94c8e9f4
yangjinlai <274948052@qq.com>
2019-12-13 14:44:34 +0800

    路径

--- gulpfile.js
+++ gulpfile.js
@@ -740,8 +740,19 @@ gulp.task("changePath", function(cb) {
     ], cb)
 })
 
+gulp.task("commonHtml", function( cb ) {
+
+    pump([
+        gulp.src(['src/common/views/**/*.html']),
+
+        gulp.dest( 'middle/js/common/views')
+
+    ], cb)
+})
+
+
 //非include文件夹下的js文件打包
-gulp.task("webpack", ['jsCpd', 'changePath'], function(cb) {
+gulp.task("webpack", ['jsCpd', 'changePath', 'commonHtml'], function(cb) {
     //测试环境
     pump([
         gulp.src(['src/**/*.js']),
@@ -821,19 +832,10 @@ function changeCommonImg(file) {
 }
 
 
-gulp.task("commonHtml", function( cb ) {
-
-    pump([
-        gulp.src(['src/common/views/**/*.html']),
-
-        gulp.dest( 'middle/js/common/views')
-
-    ], cb)
-})
 
 
 //html文件打包
-gulp.task('html', ['commonHtml'],  function(cb) {
+gulp.task('html',  function(cb) {
 
     pump([
         gulp.src(['src/**/views/**/*.html', '!src/common/views/**/*.html', '!src/newCommon/views/**/*.html']), //- 读取 rev-manifest.json 文件以及需要进行css名替换的文件
