eb20322ce3082fa6f83de4e746c5c226774544a3
yanruiting <879650384@qq.com>
2019-12-23 11:00:40 +0800

    无缝加载失败数据处理

--- src/newCommon/js/ajaxLoading.js
+++ src/newCommon/js/ajaxLoading.js
@@ -116,7 +116,7 @@ var splitUrl = require('./components/splitUrl.js')();
                 needDataEmpty: true, //需要判断data是否为空
                 needLoading: true, //需要显示loading遮罩
                 callbackDone: function() {},
-                callbackFail: function() {},
+                callbackFail: null,
                 callbackNoData: function() {},
                 //formData
                 formData: false, //判断是否需要使用formData上传
