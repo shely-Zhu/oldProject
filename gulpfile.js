5afe63a5053f93a53e79e51c29f715cb592eca9b
zhaoziyun <zhaoziyun@chtwm.com>
2020-01-02 14:07:45 +0800

    tijiaodaoma

--- gulpfile.js
+++ gulpfile.js
@@ -1043,7 +1043,7 @@ gulp.task('commonImages', function( cb ) {
         gulp.dest(host.path + 'rev/allServerResources/include/commonImg/')
 
         //预上线环境时，去掉Log并压缩
-        plugins.if(options.env === '3' || options.env === '4', plugins.removelogs()),
+        //plugins.if(options.env === '3' || options.env === '4', plugins.removelogs()),
         // plugins.if(options.env === '3' || options.env === '4', plugins.uglify({ //压缩
         //     mangle: false, //类型：Boolean 默认：true 是否修改变量名
         //     compress: false
