3fc18155b7d210606e56c08b0ee36da1d63a5ce2
caoqihai <137594357@qq.com>
2020-01-06 19:35:04 +0800

    '改bug654'

--- data/wap/pof/fundDiagnosis/queryAllByCustomerNo.js
+++ data/wap/pof/fundDiagnosis/queryAllByCustomerNo.js
@@ -7,23 +7,27 @@ var data = Mock.mock({
 			"applyDate": 1576049561000,
 			"applyStatus": 1,
 			"customerNo": "464102",
-			"id": 20
+			"id": 20,
+			"applyDateStr": "2019-12-15 16:24"
 		}, {
 			"applyDate": 623453245,
-			"applyStatus": 1,
+			"applyStatus": 2,
 			"customerNo": "464102",
-			"id": 12
+			"id": 12,
+			"applyDateStr": "2019-12-16 16:24"
 		}],
 		"2910年11月":[{
 			"applyDate": 1576049561000,
-			"applyStatus": 1,
+			"applyStatus": 3,
 			"customerNo": "464102",
-			"id": 20
+			"id": 20,
+			"applyDateStr": "2019-11-15 16:24"
 		}, {
 			"applyDate": 623453245,
-			"applyStatus": 1,
+			"applyStatus": 4,
 			"customerNo": "464102",
-			"id": 12
+			"id": 12,
+			"applyDateStr": "2019-11-16 16:24"
 		}]
 	},
 	"message": "操作成功！",
