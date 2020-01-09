11dea71a182730d15d856273841b90f951b0d73b
webwangjia <986785991@qq.com>
2020-01-09 10:12:37 +0800

    买入结果页得ui修改

--- src/financial/static/js/publicPlacement/cashTransformOut.js
+++ src/financial/static/js/publicPlacement/cashTransformOut.js
@@ -129,14 +129,16 @@ $(function () {
 					},
 					needDataEmpty:true,
 					callbackDone:function(json){
-						console.log("88888")
 						that.gv.ruleList = json.data;
 						that.$e.el_transformRule[0].textContent = that.gv.ruleList[2].title;
 						that.$e.el_transformRule.attr("ruleId",that.gv.ruleList[2].id);
 						that.$e.el_transformRule_icon.attr("ruleId",that.gv.ruleList[2].id);
-
 						that.$e.el_agreementRule[0].textContent = that.gv.ruleList[1].title;
 						that.$e.el_agreementRule.attr('ruleId',that.gv.ruleList[1].id);
+						
+						// if(that.$e.el_transformRule[0].textContent!=""){
+						// 	$(".cashRight").show()
+						// }
 					}
 				}
 			]
