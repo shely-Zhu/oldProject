41d2b5e855afa1ed010d698bf0cd344a6a7fe736
peicongcong <2256757696@qq.com>
2020-01-10 20:52:02 +0800

    '样式修改'

--- src/financial/static/js/publicPlacement/myInvestmentPlan.js
+++ src/financial/static/js/publicPlacement/myInvestmentPlan.js
@@ -15,14 +15,14 @@ require('@pathCommonJsCom/goTopMui.js');
 var generateTemplate = require('@pathCommonJsComBus/generateTemplate.js');
 var tipAction = require('@pathCommonJs/components/tipAction.js');
 require('@pathCommonCom/pullRefresh/pullRefresh.js');
-$(function () {
+$(function() {
     var somePage = {
         $e: {
             recordList: $('.contentWrap'), // 调仓记录
             investmentPlanTemp: $('#investmentPlan-template'), // 最新调仓模板
             noData: $(".noData"),
-            endPlan: $(".endPlan"),  //已终止的定投计划
-            stopPlan:$(".stoppPlan"), //已暂停的定投计划
+            endPlan: $(".endPlan"), //已终止的定投计划
+            stopPlan: $(".stoppPlan"), //已暂停的定投计划
             listLoading: $('.listLoading'), //所有数据区域，第一次加载的loading结构
 
         },
@@ -30,159 +30,163 @@ $(function () {
             pageCurrent: 1, //当前页码，默认为1
             pageSize: 10,
             listLength: 0,
-            fixStateNum : 0,
-            stopNum:0,//暂停的定投计划数
-            stopPlanList:[],    // 已终止的定投计划
-            stopPlanList_1:[],  //已暂停的定投计划
-            accountType:"",
-            paddingStatus:false,  //有进行中的计划状态
+            fixStateNum: 0,
+            stopNum: 0, //暂停的定投计划数
+            stopPlanList: [], // 已终止的定投计划
+            stopPlanList_1: [], //已暂停的定投计划
+            accountType: "",
+            paddingStatus: false, //有进行中的计划状态
         },
-        init: function () {
+        init: function() {
             var that = this;
             that.initMui();
             that.getUserInfo();
             that.events();
         },
         //初始化mui的上拉加载
-        initMui: function () {
+        initMui: function() {
             var that = this;
             var height = windowHeight - $(".newPlan").height() - $(".topTitle").height();
             if (!$('.list').hasClass('setHeight')) {
                 $('.list').height(height).addClass('setHeight');
             }
             $.pullRefresh({
-                wrapper: $('.list'),
-                class: 'listItem',
-                template: that.$e.investmentPlanTemp, 
-                pageSize: that.gV.pageSize,
-                callback: function(def, t){
-                    var obj = [{
-                        url: site_url.protocolList_api,
-                        data: {
-                            "pageNo": that.gV.pageCurrent, //非必须，默认为1
-                            "pageSize": 10,//非必须，默认为10
-                        },
-                        needDataEmpty: true,
-                        needLoading: false,
-                        callbackDone: function(json) {    
-                            var data = json.data.pageList;
-                            if(that.gV.pageCurrent == 1 && data.length == 0) {
-                                $(".list").css("display", "none");
-                                that.$e.noData.show();
-                            } else {
-                                for (var i = 0; i < data.length; i++) {
-                                    if (data[i].fixState == 'A') {
-                                        data[i].fixStateStr = "进行中"
-                                        data[i].show = true
-                                        that.gV.paddingStatus = true
-                                    } else if (data[i].fixState == 'H') {
-                                        data[i].fixStateStr = "已终止"
-                                        data[i].show = false
-                                        that.gV.fixStateNum ++
-                                        that.gV.stopPlanList.push(data[i])
-                                    } else {
-                                        data[i].fixStateStr = "暂停"
-                                        data[i].show = true
-                                        that.gV.stopNum ++
-                                        that.gV.stopPlanList_1.push(data[i])
-                                    }
-                                    if(data[i].totalTradeTimes.length == 0){
-                                        data[i].totalTradeTimes_s = false
-                                    }else{
-                                        data[i].totalTradeTimes_s = true
-                                    }
-                                }
-                                if (that.gV.fixStateNum > 0) {
-                                    that.$e.endPlan.show()
-                                    var height = windowHeight - $(".newPlan").height() - $(".topTitle").height() - $(".endPlan").height();
-                                    $('.list .contentWrapper').height(height)
-                                    $(".stopPlan").html(that.gV.fixStateNum)
-
-                                } else {
-                                    that.$e.endPlan.hide()
-                                }
-                                if(that.gV.stopNum>0){
-                                    //that.$e.stopPlan.show()
-                                    var height = windowHeight - $(".newPlan").height() - $(".topTitle").height() - $(".endPlan").height();
-                                   // $('.list .contentWrapper').height(height)
-                                   // $(".stopPlan_1").html(that.gV.stopNum)
-                                }else{
-                                    that.$e.stopPlan.hide()
-                                }
-                                if(!that.gV.paddingStatus&&that.gV.stopNum==0){
+                    wrapper: $('.list'),
+                    class: 'listItem',
+                    template: that.$e.investmentPlanTemp,
+                    pageSize: that.gV.pageSize,
+                    callback: function(def, t) {
+                        var obj = [{
+                            url: site_url.protocolList_api,
+                            data: {
+                                "pageNo": that.gV.pageCurrent, //非必须，默认为1
+                                "pageSize": 10, //非必须，默认为10
+                            },
+                            needDataEmpty: true,
+                            needLoading: false,
+                            callbackDone: function(json) {
+                                var data = json.data.pageList;
+                                if (that.gV.pageCurrent == 1 && data.length == 0) {
                                     $(".list").css("display", "none");
                                     that.$e.noData.show();
-                                    return ;
-                                }
-                                def && def.resolve( data, that.gV.pageCurrent); 
-                                if (that.gV.pageCurrent == 1) {
-                                    for (var i = 0; i < data.length; i++) {
-                                        if (data[i].fixStateStr == "暂停") {
-                                            $(".content-t span").eq(i).addClass("suspend")
-                                        }
-                                    }
                                 } else {
                                     for (var i = 0; i < data.length; i++) {
-                                        if (data[i].fixStateStr == "暂停") {
-                                            $(".content-t span").eq(i + 15 * that.gV.pageCurrent - 15).addClass("suspend")
+                                        if (data[i].fixState == 'A') {
+                                            data[i].fixStateStr = "进行中"
+                                            data[i].show = true
+                                            that.gV.paddingStatus = true
+                                        } else if (data[i].fixState == 'H') {
+                                            data[i].fixStateStr = "已终止"
+                                            data[i].show = false
+                                            that.gV.fixStateNum++
+                                                that.gV.stopPlanList.push(data[i])
+                                        } else {
+                                            data[i].fixStateStr = "暂停"
+                                            data[i].show = true
+                                            that.gV.stopNum++
+                                                that.gV.stopPlanList_1.push(data[i])
+                                        }
+                                        if (data[i].totalTradeTimes.length == 0) {
+                                            data[i].totalTradeTimes_s = false
+                                        } else {
+                                            data[i].totalTradeTimes_s = true
                                         }
                                     }
+                                    if (that.gV.fixStateNum > 0) {
+                                        that.$e.endPlan.show()
+                                        var height = windowHeight - $(".newPlan").height() - $(".topTitle").height() - $(".endPlan").height();
+                                        $('.list .contentWrapper').height(height)
+                                        $(".stopPlan").html(that.gV.fixStateNum)
+
+                                    } else {
+                                        that.$e.endPlan.hide()
+                                    }
+                                    if (that.gV.stopNum > 0) {
+                                        //that.$e.stopPlan.show()
+                                        var height = windowHeight - $(".newPlan").height() - $(".topTitle").height() - $(".endPlan").height();
+                                        // $('.list .contentWrapper').height(height)
+                                        // $(".stopPlan_1").html(that.gV.stopNum)
+                                    } else {
+                                        that.$e.stopPlan.hide()
+                                    }
+                                    if (!that.gV.paddingStatus && that.gV.stopNum == 0) {
+                                        $(".list").css("display", "none");
+                                        that.$e.noData.show();
+                                        return;
+                                    }
+                                    def && def.resolve(data, that.gV.pageCurrent);
+                                    if (that.gV.pageCurrent == 1) {
+                                        for (var i = 0; i < data.length; i++) {
+                                            if (data[i].fixStateStr == "暂停") {
+                                                $(".content-t span").eq(i).addClass("suspend");
+                                            } else if (data[i].fixStateStr == "已终止") {
+                                                $(".content-t span").eq(i).addClass("redpend");
+                                            }
+                                        }
+                                    } else {
+                                        for (var i = 0; i < data.length; i++) {
+                                            if (data[i].fixStateStr == "暂停") {
+                                                $(".content-t span").eq(i + 15 * that.gV.pageCurrent - 15).addClass("suspend");
+                                            } else if (data[i].fixStateStr == "已终止") {
+                                                $(".content-t span").eq(i).addClass("redpend");
+                                            }
+                                        }
+                                    }
+                                    that.gV.pageCurrent++;
                                 }
-                                that.gV.pageCurrent++;
-                            }
-                            
-                        },
-                        callbackNoData: function( json ){
-                            if(that.gV.pageCurrent == 1) {
-                                $(".list").css("display", "none")
-                                that.$e.noData.show()
-                            }
-                            def && def.reject( json, that.gV.pageCurrent );
-                        },
-                        callbackFail: function(json) {
-                            def && def.reject( json, that.gV.pageCurrent );
-                        },
-                    }];
-                    $.ajaxLoading(obj); 
+
+                            },
+                            callbackNoData: function(json) {
+                                if (that.gV.pageCurrent == 1) {
+                                    $(".list").css("display", "none")
+                                    that.$e.noData.show()
+                                }
+                                def && def.reject(json, that.gV.pageCurrent);
+                            },
+                            callbackFail: function(json) {
+                                def && def.reject(json, that.gV.pageCurrent);
+                            },
+                        }];
+                        $.ajaxLoading(obj);
+                    }
+                })
+                /*if (!$('.list .contentWrapper').hasClass('setHeight')) {
+                    $('.list .contentWrapper').height(height).addClass('setHeight');
                 }
-            })
-            /*if (!$('.list .contentWrapper').hasClass('setHeight')) {
-                $('.list .contentWrapper').height(height).addClass('setHeight');
-            }
-            mui.init({
-                pullRefresh: {
-                    container: '.contentWrapper',
-                    up: {
-                        //auto: false,
-                        contentrefresh: '拼命加载中',
-                        contentnomore: '没有更多了', //可选，请求完毕若没有更多数据时显示的提醒内容；
-                        callback: function () {
-                            //执行ajax请求
-                            // that.$e.listLoading.show();
-                            that.getData(this);
+                mui.init({
+                    pullRefresh: {
+                        container: '.contentWrapper',
+                        up: {
+                            //auto: false,
+                            contentrefresh: '拼命加载中',
+                            contentnomore: '没有更多了', //可选，请求完毕若没有更多数据时显示的提醒内容；
+                            callback: function () {
+                                //执行ajax请求
+                                // that.$e.listLoading.show();
+                                that.getData(this);
+                            }
                         }
                     }
-                }
-            });
+                });
 
-            //init后需要执行ready函数，才能够初始化出来
-            mui.ready(function () {
+                //init后需要执行ready函数，才能够初始化出来
+                mui.ready(function () {
 
-                //隐藏当前的加载中loading
-                if (!$('.list').hasClass('hasPullUp')) {
-                    $('.list').find('.mui-pull-bottom-pocket').addClass('mui-hidden');
-                }
+                    //隐藏当前的加载中loading
+                    if (!$('.list').hasClass('hasPullUp')) {
+                        $('.list').find('.mui-pull-bottom-pocket').addClass('mui-hidden');
+                    }
 
-                //显示loading
-                that.$e.listLoading.show();
+                    //显示loading
+                    that.$e.listLoading.show();
 
-                //这一句初始化并第一次执行mui上拉加载的callback函数
-                mui('.contentWrapper').pullRefresh().pullupLoading();
+                    //这一句初始化并第一次执行mui上拉加载的callback函数
+                    mui('.contentWrapper').pullRefresh().pullupLoading();
 
 
-                //为$id添加hasPullUp  class
-                $('.list').addClass('hasPullUp');
-            });*/
+                    //为$id添加hasPullUp  class
+                    $('.list').addClass('hasPullUp');
+                });*/
         },
         /*getData: function (t) {
             var that = this;
@@ -306,35 +310,35 @@ $(function () {
             }]
             $.ajaxLoading(obj);
         },
-        events: function () {
+        events: function() {
             var that = this;
             //新增 跳原生定投排行页
-            mui("body").on("mdClick", ".newPlan", function () {
-                if(that.gV.accountType == 0 ||that.gV.accountType == 2){
+            mui("body").on("mdClick", ".newPlan", function() {
+                if (that.gV.accountType == 0 || that.gV.accountType == 2) {
                     tipAction("暂不支持机构客户进行交易")
                     return
                 }
                 window.location.href = site_url.investmentPlanRanking_url + '?flag=2';
             }, {
-				htmdEvt: 'myInvestmentPlan_01'
-			});
+                htmdEvt: 'myInvestmentPlan_01'
+            });
 
             // 跳转详情页
-            mui("body").on("mdClick", ".investmentPlan-item", function (e) {
+            mui("body").on("mdClick", ".investmentPlan-item", function(e) {
                 var scheduledProtocolId = $(this).data('id');
                 window.location.href = site_url.pofCastSurelyDetails_url + '?scheduledProtocolId=' + scheduledProtocolId;
             }, {
-				htmdEvt: 'myInvestmentPlan_02'
+                htmdEvt: 'myInvestmentPlan_02'
             });
-            
+
             //跳往已终止的定投计划
-            mui("body").on("mdClick", ".goEndPlan", function () {
-                window.location.href = site_url.myInvestmentPlanH_url ;
-            
-               // sessionStorage.setItem('stopList',JSON.stringify(that.gV.stopPlanList))
+            mui("body").on("mdClick", ".goEndPlan", function() {
+                window.location.href = site_url.myInvestmentPlanH_url;
+
+                // sessionStorage.setItem('stopList',JSON.stringify(that.gV.stopPlanList))
             }, {
-				htmdEvt: 'myInvestmentPlan_03'
-			});
+                htmdEvt: 'myInvestmentPlan_03'
+            });
 
         }
     };
