5f2e49a194927b4b453e41d8497df6070c59eb2d
songxiaoyuname <3086751484@qq.com>
2020-01-14 21:38:01 +0800

    公募页面优化

--- gulpfile.js
+++ gulpfile.js
@@ -223,8 +223,8 @@ gulp.task('proxyTask', function() {
                     // target: 'https://app.htjf4.com',
                     // target: 'http://172.16.187.129:8080',//李亚楠
                     // target: 'http://192.168.50.254:8085',
-                    target: 'https://app.chtfundtest.com',
-                    // target:"https://app.haomaojf.com",
+                    // target: 'https://app.chtfundtest.com',
+                    target:"https://app.haomaojf.com",
                     changeOrigin: true,
                     secure: false,
                 }),
@@ -233,8 +233,8 @@ gulp.task('proxyTask', function() {
                     // target: 'https://h5.htjf4.com',
                     //  target: 'http://172.16.187.129:8080',//李亚楠
                     // target: 'http://172.16.187.164:8081',
-                    target: 'https://h5.chtfundtest.com',
-                    // target:"https://h5.haomaojf.com",
+                    // target: 'https://h5.chtfundtest.com',
+                    target:"https://h5.haomaojf.com",
                     changeOrigin: true,
                     secure: false,
                 }),
