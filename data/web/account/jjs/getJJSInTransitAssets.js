d7a35429a96796b4257608b693bf94b44cdc5d55
yanruiting <879650384@qq.com>
2020-02-28 02:35:38 +0800

    还原待确认资产mock数据

--- data/web/account/jjs/getJJSInTransitAssets.js
+++ data/web/account/jjs/getJJSInTransitAssets.js
@@ -12,8 +12,8 @@ var mymessage = Mock.mock({
     data: {
         "totalInTransitAsset": '200000', // 在途资产汇总
         "totalInTransitAssetMask": '200,00', // 在途资产汇总 千分位
-        "pageList|1": [{
-            "projectName": "（DZ银杏40号）恒天财富隐裕26号私募投资基金（第三十六次开放）", // 项目名称
+        "pageList|20": [{
+            "projectName": "项目名称", // 项目名称
             "besTime": "50.55", // 预约时间
             "besTimeStr": "-----", // 预约时间 yyyy-MM-dd格式
             "besAmount": "12.45", // 预约金额
@@ -21,7 +21,7 @@ var mymessage = Mock.mock({
             "transAmount": "16.25", // 到账金额
             "transAmountMask": "16.25", // 到账金额 千分位
             "transAmountMask": "16.25", // 到账金额 千分位
-            "accountStatus": true, // 到账状态
+            "accountStatus": "45.000.00", // 到账状态
             "transDate": "45.000.00", // 到账时间
             "transDate": "45.000.00", // 到账时间
             "transDateStr": "20.75" // 到账时间 yyyy-MM-dd格式
