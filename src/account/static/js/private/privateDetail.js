661f0b9c2e5af7e6b2841328c0723ad3b90df4d3
chentiancheng <1934024095@qq.com>
2020-04-22 20:24:24 +0800

    提交代码

--- src/account/static/js/private/privateDetail.js
+++ src/account/static/js/private/privateDetail.js
@@ -442,8 +442,8 @@ $(function() {
 			    },
 			    grid:{
 					top: '10%',
-			    	left: '5%',
-			    	right: '8%',
+			    	left: '4%',
+			    	right: '10%',
 			    	bottom: '10%',
 			    	containLabel: true
 			    },
